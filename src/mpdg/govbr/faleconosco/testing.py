c6db38d0ee7cae5e250d62f08c2124da1e7dc114
Bruno Barbosa <bruno.bernardo@planejamento.gov.br>
2017-04-20 17:19:46 -0300

    prepare product for automated testing

--- src/mpdg/govbr/faleconosco/testing.py
+++ src/mpdg/govbr/faleconosco/testing.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# from plone.app.contenttypes.testing import PLONE_APP_CONTENTTYPES_FIXTURE
 from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import applyProfile
 from plone.app.testing import FunctionalTesting
@@ -17,10 +16,10 @@ class MpdgGovbrFaleconoscoLayer(PloneSandboxLayer):
         # The z3c.autoinclude feature is disabled in the Plone fixture base
         # layer.
         import mpdg.govbr.faleconosco
-        self.loadZCML(package=mpdg.govbr.faleconosco)
+        self.loadZCML(name='testing.zcml', package=mpdg.govbr.faleconosco)
 
     def setUpPloneSite(self, portal):
-        self.applyProfile(portal, 'mpdg.govbr.faleconosco:default')
+        self.applyProfile(portal, 'mpdg.govbr.faleconosco:testing')
 
 
 MPDG_GOVBR_FALECONOSCO_FIXTURE = MpdgGovbrFaleconoscoLayer()
@@ -30,19 +29,3 @@ MPDG_GOVBR_FALECONOSCO_INTEGRATION_TESTING = IntegrationTesting(
     bases=(MPDG_GOVBR_FALECONOSCO_FIXTURE,),
     name='MpdgGovbrFaleconoscoLayer:IntegrationTesting'
 )
-
-
-# MPDG_GOVBR_FALECONOSCO_FUNCTIONAL_TESTING = FunctionalTesting(
-#     bases=(MPDG_GOVBR_FALECONOSCO_FIXTURE,),
-#     name='MpdgGovbrFaleconoscoLayer:FunctionalTesting'
-# )
-
-
-# MPDG_GOVBR_FALECONOSCO_ACCEPTANCE_TESTING = FunctionalTesting(
-#     bases=(
-#         MPDG_GOVBR_FALECONOSCO_FIXTURE,
-#         REMOTE_LIBRARY_BUNDLE_FIXTURE,
-#         z2.ZSERVER_FIXTURE
-#     ),
-#     name='MpdgGovbrFaleconoscoLayer:AcceptanceTesting'
-# )
